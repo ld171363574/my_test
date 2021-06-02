deb4796c9fe2670d8d59067b43a6a08e1d7388b9
ld171363574 <58286310+ld171363574@users.noreply.github.com>
2021-06-02 11:01:11 +0800

    Update test001.py

--- test001.py
+++ test001.py
@@ -1,1 +1,6 @@
-123123
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
+driver.find_element_by_id("kw").send_keys("自动化测试")
